5b4ae3566e721a273b29dac16f1d76c66ac4b9ab
zero67refrigeracao-web <zero67refrigeracao@gmail.com>
2026-01-12 21:29:42 -0300

    Delete api/status.js

--- api/status.js
+++
@@ -1,8 +0,0 @@
-const { kv } = require('@vercel/kv');
-
-module.exports = async (req, res) => {
-  res.status(200).json({
-    ok: true,
-    message: 'Backend de IA na Vercel funcionando.'
-  });
-};
